156c7eeb9784da9341279f785ef4e4981f9d296d
namanjain-github <43693548+namanjain-github@users.noreply.github.com>
2020-12-20 00:13:52 +0530

    Add files via upload
    This is Drum kit game, basically developed with the help of HTML, CSS, JS . In our childhood we have played drum game which produces some certain sound by pressing some keys. This one is exactly same, hope you guys enjoy and get to know the logic behind its dev!!

---
+++ index.js
@@ -0,0 +1,82 @@
+
+//document.querySelector("button").addEventListener("click", handleClick);
+
+//function handleClick(){
+    //alert("I got clicked");
+//}
+
+//document.querySelector("button").addEventListener("click", function(){   (This is called the Anormous Function in which we dont have to make specific function as mention above).
+    //alert("I got clicked");
+    //});
+
+    //document.querySelector("button")[0].addEventListener("click", handleClick);
+    //document.querySelector("button")[1].addEventListener("click", handleClick);
+    //function handleClick(){
+        //alert("I got clicked");
+    //}
+    var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+    for(var i=0 ; i < numberOfDrumButtons ; i++){
+        document.querySelectorAll(".drum")[i].addEventListener("click",function()
+        {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+        });
+    }
+    document.addEventListener("keypress",function(event){
+        makeSound(event.key);
+        buttonAnimation(event.key);
+    });
+
+    function makeSound(key){
+
+        switch (key) {
+            case "w":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();   
+                break;
+            
+                case "a":
+                    var audio = new Audio('sounds/tom-2.mp3');
+                    audio.play();   
+                    break;
+
+                case "s":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();   
+                break;
+
+                case "d":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();   
+                break;
+
+                case "j":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();   
+                break;
+
+                case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();   
+                break;
+
+                case "l":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();   
+                break;
+            default: console.log(buttonInnerHTML);
+        }
+    }
+
+
+    function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed")
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100);
+
+    }
